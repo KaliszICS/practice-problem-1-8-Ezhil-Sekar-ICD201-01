c61577a75ba82b099b91c0d436c41c35901035a4
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-26 14:18:14 +0000

    e

--- PP1_8.py
+++ PP1_8.py
@@ -1,19 +1,27 @@
 
 def q1():
   #Write Assignment code here
-
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+  print(bool1 or bool2)
 def q2():
   #Write Assignment code here
-
+  user = int(input( "Enter an integer: "))
+  print(user !=0)
 def q3():
   #Write Assignment code here
-
+  user = int(input("Enter a number: "))
+  print(user>0 and user<10)
 def q4():
   #Write Assignment code here
-
+  user = input("Input food: ")
+  user2 = input("Input drink: ")
+  print(not user=='pizza' or user2=='drink')
 def q5():
   #Write Assignment code here
-
+  user = int(input("Enter an integer: "))
+  print(f'The integer {user} is {user%2==0}')
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
